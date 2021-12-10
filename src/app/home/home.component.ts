8aee0265168fcb3102a3b09c214f43efe9f078cd
sidhant <sidhantsharma293@gmail.com>
2021-12-11 01:01:26 +0530

    navbar responsive done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  burgerToogle:boolean;
+
+  constructor() { 
+    this.burgerToogle=true;
+  }
 
   ngOnInit(): void {
   }
